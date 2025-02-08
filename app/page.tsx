59c35c934b4ca701b2dd8776ddc8a2de0dfb9724
jiahua-cui <110630464+jiahua-cui@users.noreply.github.com>
2025-02-08 18:09:07 -0500

    added pfp and updated style

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <main className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20" >
-        <section className="flex flex-col gap-8 row-start-2 justify-items-center items-center sm:items-start">
+      <main className="items-center relative justify-items-center min-h-screen p-8 pb-20 gap-16 sm:ps-10" >
+        <Image
+          src="/pfp.png"
+          alt="File icon"
+          className="rounded-full fixed left-15 top-20"
+          width={150}
+          height={150}
+        />
+        <section className="flex flex-col relative gap-8 row-start-2 justify-items-center items-center sm:items-start">
           <div className="flex gap-4 items-center flex-col">
             <h1 className="text-gray-200 text-center tracking-tighter display-1 mt-20">Jiahua Cui</h1>
             <h1 className="text-gray-200 text-center tracking-tighter display-1">Software <span className="block">Developer</span></h1>
+            <i className="bi bi-chevron-double-down absolute -bottom-20 text-2xl"></i>
+          </div>
+        </section>
+        <section className="flex flex-col gap-8 row-start-2 justify-items-center items-center mt-40 sm:items-start">
+          <div className="flex gap-4 items-center flex-col">
+            <h2 className="text-gray-200 text-center tracking-tighter display-2 mt-20">Stats</h2>
           </div>
         </section>
       </main>
